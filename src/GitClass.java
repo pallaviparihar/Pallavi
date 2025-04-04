c50903b23ae291fb1f107e3730c90bf439e58c1c
Pallavi Parihar <pallavi.parihar.11@gmail.com>
2025-04-04 11:04:32 +0530

    comiting new changes

---
+++ src/GitClass.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		String arr1[]={"Pallavi","Singh","parihar"};
+
+		System.out.println("Printing Array: " + Arrays.toString(arr1));
+		List<String> list=new ArrayList<>();
+		for(String lang:arr1){
+		list.add(lang);
+		}
+		System.out.println("Printing List: "+list);
+
+		}
+
+	}
+
+
